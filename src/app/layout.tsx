f25b0bc6fa3e4a579f0e37e000078030f5060ea7
MrEpoch <alexandrstencuk@seznam.cz>
2024-07-23 14:56:04 +0200

    feat: Removed darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Providers } from "./provider";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -20,13 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
           <main className="min-h-screen w-full dark:bg-darkmode-500">
             <Header />
             {children}
             <Footer />
           </main>
-        </Providers>
       </body>
     </html>
   );
